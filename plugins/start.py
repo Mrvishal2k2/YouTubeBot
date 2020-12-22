45b11dbc83f133ca6a59f562a1e8f44abbc47560
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2020-12-22 11:24:32 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,10 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("Channel", url="https://t.me/whiteeyebots")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "Report Bugs 😊", url="https://t.me/mrdayamzaidibot")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n I Am WhiteEye-YouTubeBot I Can Download Any Video Of Youtube Do /help for More info. Join The Channel Given below For My Use"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
