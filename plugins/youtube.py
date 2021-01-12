cc53264ce01ed08a31dedc2ac8119a2a6a6faa87
Dayam Zaidi <69107551+mrdayamzaidi@users.noreply.github.com>
2021-01-12 17:05:33 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -37,30 +37,30 @@ async def ytdl(_, message):
               return
         except:
             pass
-      url = message.text.strip()
-      await message.reply_chat_action("typing")
-      try:
-          title, thumbnail_url, formats = extractYt(url)
+        url = message.text.strip()
+        await message.reply_chat_action("typing")
+        try:
+            title, thumbnail_url, formats = extractYt(url)
 
-          now = datetime.now()
-          user_time[message.chat.id] = now + \
-                                       timedelta(minutes=youtube_next_fetch)
+            now = datetime.now()
+            user_time[message.chat.id] = now + \
+                                         timedelta(minutes=youtube_next_fetch)
 
-      except Exception:
-          await message.reply_text("`Failed To Fetch Youtube Data... 😔 \nPossible Youtube Blocked server ip \n#error`")
-          return
-      buttons = InlineKeyboardMarkup(list(create_buttons(formats)))
-      sentm = await message.reply_text("Processing Youtube Url 🔎 🔎 🔎")
-      try:
+        except Exception:
+            await message.reply_text("`Failed To Fetch Youtube Data... 😔 \nPossible Youtube Blocked server ip \n#error`")
+            return
+        buttons = InlineKeyboardMarkup(list(create_buttons(formats)))
+        sentm = await message.reply_text("Processing Youtube Url 🔎 🔎 🔎")
+        try:
         # Todo add webp image support in thumbnail by default not supported by pyrogram
         # https://www.youtube.com/watch?v=lTTajzrSkCw
-          await message.reply_photo(thumbnail_url, caption=title, reply_markup=buttons)
-          await sentm.delete()
-      except Exception as e:
-          try:
-              thumbnail_url = "https://telegra.ph/file/ce37f8203e1903feed544.png"
-              await message.reply_photo(thumbnail_url, caption=title, reply_markup=buttons)
-          except Exception as e:
-              await sentm.edit(
-              f"<code>{e}</code> #Error")
+            await message.reply_photo(thumbnail_url, caption=title, reply_markup=buttons)
+            await sentm.delete()
+        except Exception as e:
+            try:
+                thumbnail_url = "https://telegra.ph/file/ce37f8203e1903feed544.png"
+                await message.reply_photo(thumbnail_url, caption=title, reply_markup=buttons)
+            except Exception as e:
+                await sentm.edit(
+                f"<code>{e}</code> #Error")
 
